f970c54b1a9a77ae3536b1680931592aa87a922e
Stephen <88560465+Stphn-Wrn@users.noreply.github.com>
2026-07-05 13:20:54 +0200

    Update warhammerItemSheet.js

--- module/warhammerItemSheet.js
+++ module/warhammerItemSheet.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export class WarhammerItemSheet extends ItemSheet {
 
   static get defaultOptions() {
@@ -28,34 +27,3 @@ export class WarhammerItemSheet extends ItemSheet {
   }
 }
 
-=======
-export class WarhammerItemSheet extends ItemSheet {
-
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ['warhammer2e', 'sheet', 'item'],
-      template: 'systems/warhammer2e/templates/item/item-sheet.html',
-      width: 620,
-      height: 'auto',
-      resizable: true
-    });
-  }
-
-  getData() {
-    const data = super.getData();
-    data.name         = this.item.name;
-    data.isSort       = this.item.type === 'sort';
-    data.isTalent     = this.item.type === 'talent';
-    data.isWeapon     = this.item.type === 'weapon';
-    data.isSkill      = this.item.type === 'skill';
-    data.isAmmunition = this.item.type === 'ammunition';
-    data.imgDisplay   = this.item.img || 'icons/svg/book.svg';
-    return data;
-  }
-
-  async _updateObject(event, formData) {
-    return this.item.update(foundry.utils.expandObject(formData));
-  }
-}
-
->>>>>>> 7e4ff19 (fix: talents)
